d72759c7dd0194d75ab1ea26525883eb842748b4
Selim <selimchemutai19@gmail.com>
2021-11-06 10:37:30 +0300

    Create NewlistViewModel.kt

---
+++ app/src/main/java/com/pamela/database/ui/add/NewlistViewModel.kt
@@ -0,0 +1,26 @@
+package com.pamela.database.ui.add
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.pamela.database.ListItem
+import com.pamela.database.Lists
+
+
+class NewlistViewModel : ViewModel() {
+
+    private var _listItem = MutableLiveData<ListItem>().apply {
+        value = ListItem(Lists.size.toLong(), "List Name", mutableListOf())
+    }
+    var listItem: LiveData<ListItem> = _listItem
+    fun setTitle(title:String) {
+        _listItem.postValue(ListItem(Lists.size.toLong(), title, _listItem.value!!.elements))
+    }
+    fun addItem(item:String){
+        Log.e("test", item)
+        val elements = _listItem.value!!.elements
+        elements.add(item)
+        _listItem.postValue(ListItem(Lists.size.toLong(), _listItem.value!!.title, elements))
+    }
+}
